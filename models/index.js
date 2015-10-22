afe54611724878545979d0af82d3b10b71466447
natashaslprice <natashaslprice@gmail.com>
2015-10-22 11:02:14 -0700

    ready for heroku deploy attempt #1

--- models/index.js
+++ models/index.js
@@ -1,5 +1,7 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/my_heroku_app");
+mongoose.connect(process.env.MONGOLAB_URI ||
+                 process.env.MONGOHQ_URL || 
+                 "book-search");
 
 // After creating a new model, require and export it:
 // module.exports.Tweet = require("./tweet.js");
--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/', function(req, res) {
   res.render("index");
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("express-heroku-starter is running on port 3000");
 });
 
